8b1f81e673fd2b5bd62904f33f4d6f046e03b0dd
Nicolas BARBET <NBA24700@open-groupe.com>
2025-03-04 15:44:39 +0100

    initial commit

---
+++ src/test/java/com/nico/dockndeploy/DockndeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nico.dockndeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockndeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
